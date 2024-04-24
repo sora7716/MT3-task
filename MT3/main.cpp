58b9e2a4c59220dd9f98943319ea09f67b9097c2
sora <147121795+sora7716@users.noreply.github.com>
2024-04-24 21:05:01 +0900

    セーブ

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -13,7 +13,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-
+	
 	Vector3 scale{ 1.2f,0.79f,-2.1f };
 	Vector3 rotate{ 0.4f,1.43f,-0.8f };
 	Vector3 translate{ 2.7f,-4.15f,1.57f };
