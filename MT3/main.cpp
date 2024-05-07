dcca6ab197f95436ddc87e1283ee65190ccaa7a6
sora <147121795+sora7716@users.noreply.github.com>
2024-05-07 12:50:43 +0900

    Update main.cpp

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -13,11 +13,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
-	
-	Vector3 scale{ 1.2f,0.79f,-2.1f };
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };
-	Vector3 translate{ 2.7f,-4.15f,1.57f };
-	Matrix4x4 worldMatrix = Math::MakeAffineMatrix(scale,rotate,translate);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -31,7 +26,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-	
+		Matrix4x4 orthographicMatrix = Math::MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);//正射影行列
+		Matrix4x4 perspectiveFovMatrix = Math::MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);//透視投影行列
+		Matrix4x4 viewportMatrix = Math::MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);//ビューポートマトリックス
 		///
 		/// ↑更新処理ここまで
 		///
@@ -39,7 +36,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		ScreenPrintf::MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		ScreenPrintf::MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");//正射影行列
+		ScreenPrintf::MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");//透視投影行列
+		ScreenPrintf::MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");//ビューポートマトリックス
 		///
 		/// ↑描画処理ここまで
 		///
